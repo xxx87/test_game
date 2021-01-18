6905266ae9379ac56a3f50525390ebda5da6c8b3
is.home-pc.lin <ii87@ukr.net>
2021-01-19 01:14:11 +0200

    working...

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -10,6 +10,33 @@
         </li>
       </ul>
     </div>
+    <v-card-actions>
+              <v-list-item class="grow">
+                <v-list-item-avatar color="grey darken-3">
+                  <v-img
+                    class="elevation-6"
+                    alt=""
+                    src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+                  ></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <v-list-item-title>Evan You</v-list-item-title>
+                </v-list-item-content>
+
+                <v-row align="center" justify="end">
+                  <v-icon class="mr-1">
+                    mdi-heart
+                  </v-icon>
+                  <span class="subheading mr-2">256</span>
+                  <span class="mr-1">·</span>
+                  <v-icon class="mr-1">
+                    mdi-share-variant
+                  </v-icon>
+                  <span class="subheading">45</span>
+                </v-row>
+              </v-list-item>
+            </v-card-actions>
   </div>
 </template>
 
@@ -53,7 +80,7 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 .main-app {
   position: relative;
   width: 1280px;
---
+++ src/components/PlayingField.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-container class="playground my-5">
+    <v-row no-gutters>
+      <v-col cols="7">
+        <v-card class="mx-5 my-6 rounded-xl" color="#3D4476" dark min-height="550">
+          <v-card-title>
+            <span class="">Round: 1/20</span>
+          </v-card-title>
+          <v-divider dark></v-divider>
+          <v-card-text>
+            "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="5">
+        <v-card class="ml-3" color="#30365E" dark min-height="600">
+          <v-card-title>
+            <span class="">WORDS</span>
+          </v-card-title>
+          <v-divider dark></v-divider>
+          <v-card-text>
+            <v-list color="transparent" three-line>
+              <template v-for="(item, idx) in users">
+                <v-hover :key="idx" v-slot="{ hover }" >
+                  <v-card @click="test(item.word)" color="#4E4A97" :class="{ 'on-hover': hover }" class="mb-2 rounded-xl">
+                    <v-list-item>
+                      <v-list-item-avatar size="55">
+                        <v-img :src="item.avatar"></v-img>
+                      </v-list-item-avatar>
+                      <v-list-item-title v-html="item.name"></v-list-item-title>
+                      <span class="user-words">{{ item.word.toUpperCase() }}</span>
+                    </v-list-item>
+                  </v-card>
+                </v-hover>
+              </template>
+            </v-list>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data() {
+    return {
+      users: [
+        { avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg", name: "Anna", word: "Black" },
+        { avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg", name: "Vova", word: "Dog" },
+        { avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg", name: "Lena", word: "Package" },
+        { avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg", name: "Grisha", word: "Block" },
+        { avatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg", name: "Misha", word: "Speed" }
+      ]
+    };
+  },
+  methods: {
+    test(word) {
+      let me = this;
+      console.log('TEST', word);
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+.on-hover {
+  opacity: 0.6;
+}
+
+.transparent {
+  color: rgba(255, 255, 255, 0);
+}
+.playground {
+  background-color: #232744;
+  height: 600px;
+  width: 1000px;
+  padding: 0 !important;
+}
+
+.user-words {
+  font-size: large;
+  padding: 10px;
+  border: 1px solid #000;
+  background-color: #232744;
+  border-radius: 13px;
+}
+</style>
